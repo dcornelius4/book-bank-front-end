eb3e375eeeaa841ed37ce3f1f1d4bb57f4702ed0
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-28 12:31:28 -0700

    was able to get the map to render! env help needed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+
+
+function Map() {
+  return (
+    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 45.512230, lng: -122.658722 }} />
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <>
+      <div style={{ width: '50vw', height: '50vh' }}>
+        <WrappedMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBNx25Q_AsI8zjVrCP8JrGYV0ztRZo5L4Q"
+          loadingElement={<div style={{ height: '100%' }} />}
+          containerElement={<div style={{ height: '100%' }} />}
+          mapElement={<div style={{ height: '100%' }} />}
+        />
+      </div>
+    </>
   );
 }
