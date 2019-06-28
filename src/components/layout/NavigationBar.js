0347d4a46756ecaa2a21d740d01e4967cf4dd12c
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-27 22:22:07 -0700

    added link to the navbar

--- src/components/layout/NavigationBar.js
+++ src/components/layout/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
 
@@ -7,7 +8,7 @@ const Styles = styled.div`
     background-color: #222;
   }
 
-  .navbar-brand, .navbar-nav .nav-link {
+  a, .navbar-brand, .navbar-nav .nav-link {
     color: #bbb;
 
     &:hover {
@@ -24,11 +25,31 @@ export const NavigationBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/library">Library</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/bookclub">Book Club</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/littlefreelib">Little Free Library</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="https://littlefreelibrary.org/ourmap/">Add</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link >
+              <Link to="/">
+                Home
+              </Link>
+            </Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>
+              <Link to="/library">
+                Library
+              </Link>
+            </Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>
+              <Link to="/bookclub">
+                Book Club
+              </Link>
+            </Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>
+              <Link to="/littlefreelib">
+                Little Free Library
+              </Link>
+            </Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link>
+              <Link to="https://littlefreelibrary.org/ourmap/">
+                Add
+              </Link>
+            </Nav.Link></Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
