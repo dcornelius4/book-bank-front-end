87301fc4f5e16362f6598080d7fdb6af0e51e6b6
dcornelius4 <d_cornelius94@yahoo.com>
2019-07-04 13:40:58 -0700

    added the get in the book api service

--- src/services/bookApi.js
+++ src/services/bookApi.js
@@ -1,3 +1,4 @@
-import { post } from './request';
+import { post, get } from './request';
 
 export const makeLibrary = library => post('/libraries', library);
+export const getLibrary = () => get('/libraries');
