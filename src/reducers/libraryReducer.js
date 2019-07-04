f0d64d313eb2cb918c57dd2896f041e90f230a63
dcornelius4 <d_cornelius94@yahoo.com>
2019-07-04 13:37:18 -0700

    added the info in the reducer to be able to fetch

--- src/reducers/libraryReducer.js
+++ src/reducers/libraryReducer.js
@@ -1,4 +1,9 @@
-import { NEW_LIBRARY, NEW_LIBRARY_PENDING } from '../actions/libraryActions';
+import {
+  NEW_LIBRARY,
+  NEW_LIBRARY_PENDING,
+  FETCH_LIBRARIES,
+  FETCH_LIBRARIES_PENDING
+} from '../actions/libraryActions';
 
 const initialState = {
   loading: false,
@@ -11,6 +16,10 @@ export default function reducer(state = initialState, action) {
       return { ...state, loading: true };
     case NEW_LIBRARY:
       return { ...state, loading: false, list: [...state.list, action.payload] };
+    case FETCH_LIBRARIES_PENDING:
+      return { ...state, loading: true };
+    case FETCH_LIBRARIES:
+      return { ...state, loading: false, list: action.payload };
     default:
       return state;
   }
